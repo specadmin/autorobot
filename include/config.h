d2aece7986c14503dca48e416b53d079fef429b6
Anton Rezanov <ton@specadmin.ru>
2022-02-28 21:20:31 +0300

    Initial commit

---
+++ include/config.h
@@ -0,0 +1,16 @@
+#ifndef CONFIG_H
+#define CONFIG_H
+//----------------------------------------------------------------------------
+#include <avr/io.h>
+//----------------------------------------------------------------------------
+
+// #define DEBUG_PORT          PORTB
+// #define DEBUG_DDR           DDRB
+// #define DEBUG_BIT           4
+// #define DEBUG_BAUD_RATE     1152000
+// #define DEBUG_EXTENDED_DUMP
+// #define RAM_GUARD_ENABLED
+
+//----------------------------------------------------------------------------
+#endif
+
